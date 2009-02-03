63a6054713449aad6016416f0eec2c6f6fd38ef2
mmtiller <mmtiller@users.sourceforge.net>
2009-02-03 11:06:37 +0000

    git-svn-id: svn://svn.code.sf.net/p/jpwsafe/code/branches/j2me-refactor@337 3f901d5b-7c5d-4663-935e-8925751b605d

---
+++ PasswordSafeSWT/src/org/pwsafe/passwordsafeswt/action/AddRecordAction.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2008-2009 David Muller <roxon@users.sourceforge.net>.
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
+package org.pwsafe.passwordsafeswt.action;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.JFacePreferences;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
+import org.pwsafe.passwordsafeswt.PasswordSafeJFace;
+import org.pwsafe.passwordsafeswt.dialog.EditDialog;
+import org.pwsafe.passwordsafeswt.dto.PwsEntryDTO;
+import org.pwsafe.passwordsafeswt.preference.JpwPreferenceConstants;
+
+/**
+ * Adds a new record to the password safe.
+ *
+ * @author Glen Smith
+ */
+public class AddRecordAction extends Action {
+
+    public AddRecordAction() {
+        super("&Add Record");
+        setAccelerator( SWT.MOD1 | 'A' );
+        setImageDescriptor(ImageDescriptor.createFromURL(this.getClass().getClassLoader().getResource("org/pwsafe/passwordsafeswt/images/tool_newbar_add.gif")));
+        setToolTipText("Add New Record");
+    }
+
+    /**
+     * @see org.eclipse.jface.action.Action#run()
+     */
+    public void run() {
+        PasswordSafeJFace app = PasswordSafeJFace.getApp();
+        PwsEntryDTO newEntry = PwsEntryDTO.fromPwsRecord(app.getPwsFile().newRecord());  
+        IPreferenceStore thePrefs = JFacePreferences.getPreferenceStore();
+		if (thePrefs.getBoolean(JpwPreferenceConstants.USE_DEFAULT_USERNAME)) {
+			newEntry.setUsername(thePrefs.getString(JpwPreferenceConstants.DEFAULT_USERNAME));
+		}
+		if (app.isTreeViewShowing()) {
+			// create new entry within existing group
+			String selectedGroup = app.getSelectedTreeGroup();
+			if (selectedGroup != null && selectedGroup.length() > 0) {
+				newEntry.setGroup(selectedGroup);
+			}
+		}
+        EditDialog ed = new EditDialog(app.getShell(), newEntry);
+        newEntry = (PwsEntryDTO) ed.open();
+        if (newEntry != null) {
+            app.addRecord(newEntry);
+        }
+
+    }
+
+}
---
+++ PasswordSafeSWT/src/org/pwsafe/passwordsafeswt/action/OptionsAction.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2008-2009 David Muller <roxon@users.sourceforge.net>.
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
+package org.pwsafe.passwordsafeswt.action;
+
+import java.io.IOException;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.preference.JFacePreferences;
+import org.eclipse.jface.preference.PreferenceDialog;
+import org.eclipse.jface.preference.PreferenceManager;
+import org.eclipse.jface.preference.PreferenceNode;
+import org.pwsafe.passwordsafeswt.PasswordSafeJFace;
+import org.pwsafe.passwordsafeswt.preference.DisplayPreferences;
+import org.pwsafe.passwordsafeswt.preference.MiscPreferences;
+import org.pwsafe.passwordsafeswt.preference.PasswordPolicyPreferences;
+import org.pwsafe.passwordsafeswt.preference.SecurityPreferences;
+import org.pwsafe.passwordsafeswt.preference.UsernamePreferences;
+import org.pwsafe.passwordsafeswt.util.UserPreferences;
+
+import com.swtdesigner.SWTResourceManager;
+
+/**
+ * Displays the options dialog to manage user preferences.
+ *
+ * @author Glen Smith
+ */
+public class OptionsAction extends Action {
+
+    public OptionsAction() {
+        super("Options...");
+    }
+
+    /**
+     * @see org.eclipse.jface.action.Action#run()
+     */
+    public void run() {
+        final PasswordSafeJFace app = PasswordSafeJFace.getApp();
+
+        // Create the preference manager
+        PreferenceManager mgr = new PreferenceManager();
+
+        // Create the nodes
+        PreferenceNode displayPrefs = new PreferenceNode("display", "Display", null, DisplayPreferences.class.getName());
+		PreferenceNode securityPrefs = new PreferenceNode("security", "Security", null, SecurityPreferences.class
+				.getName());
+		PreferenceNode passwordPolicyPrefs = new PreferenceNode("policy", "Password Policy", null,
+				PasswordPolicyPreferences.class.getName());
+		PreferenceNode usernamePrefs = new PreferenceNode("username", "Username", null, UsernamePreferences.class
+				.getName());
+        PreferenceNode miscPrefs = new PreferenceNode("misc", "Misc", null, MiscPreferences.class.getName());
+
+        // Add the nodes
+        mgr.addToRoot(displayPrefs);
+        mgr.addToRoot(securityPrefs);
+        mgr.addToRoot(passwordPolicyPrefs);
+        mgr.addToRoot(usernamePrefs);
+        mgr.addToRoot(miscPrefs);
+
+        // Create the preferences dialog
+        PreferenceDialog dlg = new PreferenceDialog(app.getShell(), mgr);
+		PreferenceDialog.setDefaultImage(SWTResourceManager.getImage(PasswordSafeJFace.class,
+				"/org/pwsafe/passwordsafeswt/images/clogo.gif"));
+
+        // Set the preference store
+        dlg.setPreferenceStore(JFacePreferences.getPreferenceStore());
+
+        // Open the dialog
+        dlg.open();
+
+        try {
+        	if (JFacePreferences.getPreferenceStore().needsSaving()) {
+        		// Be Paranoid - Save the preferences now
+        		UserPreferences.getInstance().savePreferences();
+        	}
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+       
+
+    }
+
+}
